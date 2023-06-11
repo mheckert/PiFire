9300e6813e00a0dc600c0c3e872a1131c87346c8
Garrett Smith <garrett.t.smith@gmail.com>
2023-06-11 12:02:37 -0500

    Support max31855 python-based spidev
    Replaces implementation of 'readtemp' c program with that of python

--- probes/max31855.py
+++ probes/max31855.py
@@ -15,7 +15,7 @@ Description:
  Imported Libraries
 *****************************************
 '''
-import subprocess
+import spidev
 import datetime
 from probes.base import ProbeInterface
 
@@ -24,6 +24,14 @@ from probes.base import ProbeInterface
  Class Definitions
 *****************************************
 '''
+class Max31855Result:
+    def __init__(self):
+        self.thermocouple_temp = 0
+        self.internal_temp = 0
+        self.fault = 0
+        self.scv_fault = 0
+        self.scg_fault = 0
+        self.oc_fault = 0
 
 class ReadProbes(ProbeInterface):
 
@@ -31,6 +39,7 @@ class ReadProbes(ProbeInterface):
         super().__init__(probe_info, device_info, units)
 
     def _init_device(self):
+        self.port = (0,0) #spidev0.0
         pass
 
     #Get temp with retry
@@ -44,15 +53,37 @@ class ReadProbes(ProbeInterface):
                 retries -= 1
         return None
 
+    def read_spidev(self):
+        spi = spidev.SpiDev()
+        spi.open(*self.port)
+        spi.max_speed_hz = 500000
+
+        spi_data = spi.xfer2([0x00] * 4)
+
+        spi.close()
+
+        result = Max31855Result()
+        result.thermocouple_temp = ((spi_data[0] << 8) | spi_data[1]) >> 2
+        result.internal_temp = ((spi_data[2] << 8) | spi_data[3]) >> 4
+        result.fault = 1 if (spi_data[1] & 0x01) else 0
+        result.scv_fault = 1 if (spi_data[3] & 0x04) else 0
+        result.scg_fault = 1 if (spi_data[3] & 0x02) else 0
+        result.oc_fault = 1 if (spi_data[3] & 0x01) else 0
+
+        if result.fault or result.scv_fault or result.scg_fault or result.oc_fault:
+            return None
+
+        thermocouple_temp_c = result.thermocouple_temp * 0.25
+        return thermocouple_temp_c
+
     def _get_temp(self):
-        child = subprocess.run("/home/pi/git/max31855/readtemp", capture_output=True)
-        if( child.returncode ):
+        temp_c = self.read_spidev()
+        if( temp_c is None ):
             now = str(datetime.datetime.now())
             now = now[0:19] # Truncate the microseconds
             print(str(now) + ' Error Reading Temperature.')
             return None
 
-        temp_c = float( child.stdout )
         temp_f = temp_c * 9 / 5.0 + 32
 
         return (temp_c, temp_f)
